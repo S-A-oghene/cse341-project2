7e4aaf3c62095fb7ae746bc09684ed0b8ea47cdd
S-A-oghene <agbamogunjacob@gmail.com>
2025-09-09 15:47:42 +0100

    Complete W01 project requirements and add testing file

---
+++ controllers/contacts.js
@@ -0,0 +1,86 @@
+const { ObjectId } = require("mongodb");
+const getDb = require("../data/database").getDatabase;
+
+exports.getAllContacts = async (req, res) => {
+  try {
+    const db = getDb();
+    const contacts = await db.db().collection("contacts").find().toArray();
+    res.status(200).json(contacts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getSingleContact = async (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: "Invalid contact ID" });
+    }
+    const db = getDb();
+    const contact = await db
+      .db()
+      .collection("contacts")
+      .findOne({ _id: new ObjectId(req.params.id) });
+    if (!contact) return res.status(404).json({ message: "Contact not found" });
+    res.status(200).json(contact);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.createContact = async (req, res) => {
+  const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+  if (!firstName || !lastName || !email || !favoriteColor || !birthday) {
+    return res.status(400).json({ message: "All fields required" });
+  }
+  try {
+    const db = getDb();
+    const result = await db
+      .db()
+      .collection("contacts")
+      .insertOne({ firstName, lastName, email, favoriteColor, birthday });
+    res.status(201).json({ id: result.insertedId });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.updateContact = async (req, res) => {
+  const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: "Invalid contact ID" });
+    }
+    const db = getDb();
+    const result = await db
+      .db()
+      .collection("contacts")
+      .updateOne(
+        { _id: new ObjectId(req.params.id) },
+        { $set: { firstName, lastName, email, favoriteColor, birthday } }
+      );
+    if (result.matchedCount === 0)
+      return res.status(404).json({ message: "Contact not found" });
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.deleteContact = async (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: "Invalid contact ID" });
+    }
+    const db = getDb();
+    const result = await db
+      .db()
+      .collection("contacts")
+      .deleteOne({ _id: new ObjectId(req.params.id) });
+    if (result.deletedCount === 0)
+      return res.status(404).json({ message: "Contact not found" });
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger/swagger.json');
 
-router.use('/auth', require('./auth'));
-
-// Integrate Swagger UI setup directly to avoid require issues
-router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-router.use('/users', require('./users'));
-router.use('/books', require('./books'));
-
-// Root route should be last
-router.get('/', (req, res) => {
-  // #swagger.ignore = true
-  res.redirect('/api-docs');
-});
+router.use("/contacts", require("./contacts"));
 
 module.exports = router;
